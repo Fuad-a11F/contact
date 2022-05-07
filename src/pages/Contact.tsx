35b8fba89445a5b07b780cfe5f5b62572d85d85e
Fuad-a11F <fuad.fuad.1984@list.ru>
2022-05-07 20:03:02 +0400

    Fixed warning

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,12 +3,11 @@ import React, { FC, useEffect } from "react";
 import Container from "../components/layout/Container";
 import Header from "../components/contact/Header";
 import ContactWrapper from "../components/contact/ContactWrapper";
-import { useCheckAuth } from "../shared/custom-hooks/useCheckAuth";
 import { useAppDispatch } from "../shared/custom-hooks/redux";
 import { sagaActions } from "../redux/saga/sagaAction";
 
 const Contact: FC = () => {
-  useCheckAuth();
+  // useCheckAuth();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
